f84966300b16301b9152b4925f549d8e25ef0404
YoungCheon Kim <kyt031522@gmail.com>
2024-06-21 10:08:25 +0900

    .

--- app/api/user/auth/route.ts
+++ app/api/user/auth/route.ts
@@ -7,14 +7,19 @@ import axios from 'axios';
 export async function GET(req: NextRequest) {
     const token = req.cookies.get("x_auth");
     console.log(token)
+
+    const corsHeaders = {
+      'Access-Control-Allow-Origin': 'https://notion-blog-next-j6nbqb54s-kych0912s-projects.vercel.app',
+      'Access-Control-Allow-Credentials': 'true',
+    };
     try {
         if (typeof token === 'undefined') {
-            return NextResponse.json({ message: 'Token Not Found', isLogged: false}, { status: 401 });
+            return NextResponse.json({ message: 'Token Not Found', isLogged: false}, { status: 401, headers: corsHeaders });
         }
         
         const decoded = verifyToken(token.value); // Pass the value of the token cookie
         if(typeof decoded === 'string'){
-            return NextResponse.json({ message: 'Invalid Token', isLogged: false}, { status: 401 });
+            return NextResponse.json({ message: 'Invalid Token', isLogged: false}, { status: 401, headers: corsHeaders });
         }
 
         const accessToken = decoded.id;  
@@ -26,14 +31,14 @@ export async function GET(req: NextRequest) {
         })
 
         if (userData) {
-            return NextResponse.json({user:userData.data , message: 'Token Verified',id:decoded, isLogged: true}, { status: 200 });
+            return NextResponse.json({user:userData.data , message: 'Token Verified',id:decoded, isLogged: true}, { status: 200, headers: corsHeaders });
         } else {
-            return NextResponse.json({ message: 'Invalid Token', isLogged: false}, { status: 401 });
+            return NextResponse.json({ message: 'Invalid Token', isLogged: false}, { status: 401, headers: corsHeaders });
         }
 
     } 
   catch (err) {
     console.log(err)
-    return NextResponse.json({ message: 'Server Error' }, { status: 500 });
+    return NextResponse.json({ message: 'Server Error' }, { status: 500, headers: corsHeaders });
   }
 } 
