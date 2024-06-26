6e0de4a3486bd9d4e8f1c9266ef04db6cdf02d6b
YoungCheon Kim <kyt031522@gmail.com>
2024-06-26 13:57:05 +0900

    loggedin button error

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -79,9 +79,6 @@ export default function Header(){
                                 }
                             </>
                         }
-
-                        <NotLoggedIn />
-
                     </Box>
             </AppBar>
     )
