cf1549e970f5e0d7c7cae10ec77a125d8f31dca7
YoungCheon Kim <kyt031522@gmail.com>
2024-08-10 03:18:26 +0900

    api 호출 try catch문 삭제. 호출하는 react-query에서 처리하기 때문

--- app/services/user/user.ts
+++ app/services/user/user.ts
@@ -1,18 +1,24 @@
 import axios from 'axios';
 import { AxiosError } from 'axios';
 
-export const getAuth = async () => {    
-    try {
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/user/auth`,{ headers: { "Content-Type": "multipart/form-data" },withCredentials: true });
-        return {
-            id:res.data.id,
-            isLogged:res.data.isLogged,
-            message:res.data.message,
-            user:res.data.user   
-        };
-    } catch (err) {
-        return err;
+interface TAuthReturn{
+    id:string,
+    isLogged:boolean,
+    message:string,
+    user:{
+        name:string,
+        avatar_url:string
     }
+}
+
+export const getAuth = async ():Promise<TAuthReturn>=> {    
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/user/auth`,{ headers: { "Content-Type": "multipart/form-data" },withCredentials: true });
+    return {
+        id: res.data.id,
+        isLogged: res.data.isLogged,
+        message: res.data.message,
+        user: res.data.user   
+    };
 }
 
 export const login = async (data:{
@@ -38,18 +44,14 @@ export const logout = async () => {
 }
 
 export const getAccessToken = async (code:string) => {
-    try {
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/user/getAccessToken?code=${code}`);
-        const _data = res.data;
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/user/getAccessToken?code=${code}`);
+    const _data = res.data;
 
-        //server component에서 client component로 데이터 전달 시, Promise 객체를 전달한다.
-        //하지만 Promise 객체는 JSON.stringify를 사용할 수 없다.
-        //nextjs 의 특징
-        //따라서, JSON.stringify를 사용하려면, Promise 객체를 data 객체로 변환해야한다.
-        return _data;
-    } catch (err) {
-        throw err;
-    }
+    //server component에서 client component로 데이터 전달 시, Promise 객체를 전달한다.
+    //하지만 Promise 객체는 JSON.stringify를 사용할 수 없다.
+    //nextjs 의 특징
+    //따라서, JSON.stringify를 사용하려면, Promise 객체를 data 객체로 변환해야한다.
+    return _data;
 }
 
 export const getUserInfo = async (id:string) =>{
