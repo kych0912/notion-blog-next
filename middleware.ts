4ca9a0264baa8e1189adaf2b34854c75f7ab5a99
YoungCheon Kim <kyt031522@gmail.com>
2024-09-05 00:17:33 +0900

    middleware 수정

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,14 @@
-// middleware.js
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
+export { default } from "next-auth/middleware";
+
+export const config = { matcher: ["/write"] };
+
 export async function middleware(request: NextRequest) {
-    const requestHeaders = new Headers(request.headers);
-    
-    // 사용자 정의 헤더 설정
-    requestHeaders.set('access-control-allow-headers', 'next-auth.session-token');
+	const token = await getToken({ req: request });
 
-    // NextResponse 객체 생성
-    const response = NextResponse.next({
-        request: {
-            headers: requestHeaders,
-        },
-    });
-
-    // /write 경로에서만 토큰 확인
-    if (request.nextUrl.pathname === '/write') {
-        const token = await getToken({ req: request });
-
-        if (!token && process.env.NEXTAUTH_URL) {
-            // 인증되지 않은 경우 리다이렉트
-            return NextResponse.redirect(process.env.NEXTAUTH_URL);
-        }
-    }
-
-    return response; // 수정된 요청 헤더와 함께 응답 반환
+	if (!token && process.env.NEXTAUTH_URL) {
+		return NextResponse.redirect(process.env.NEXTAUTH_URL);
+	}
 }
-
-// 모든 경로에 대해 미들웨어 적용
-export const config = {
-    matcher: ['/', '/write'],
-};
