1a891789ec52c1736366b05234c76e807474cb5b
YoungCheon Kim <kyt031522@gmail.com>
2024-09-19 16:17:58 +0900

    layout 컴포넌트 추가

---
+++ app/(pages)/[user]/[id]/layout.tsx
@@ -0,0 +1,13 @@
+import WithHeader from "@/app/Layout/WithHeader";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      {children}
+    </>
+  );
+}
