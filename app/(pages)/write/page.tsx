50443d671ac170171c8f22d1c18f41ffbab41b15
YoungCheon Kim <kyt031522@gmail.com>
2024-11-11 15:38:20 +0900

    write page 구성 변경

--- app/(pages)/write/page.tsx
+++ app/(pages)/write/page.tsx
@@ -1,50 +1,13 @@
-import NotionPageContainer from "./_pages/NotionPageContainer"
-import NotionPageContent from "./_pages/NotionPageContent";
-import { Suspense } from "react";
-import {
-    WriteFunnelContainer
-} from "./write.styles";
+import NotionPageContent from "./_pages/RightComponents/NotionPageContent";
+import NotionUrlInput from "./_pages/LeftComponents/NotionUrlInput";
 
-
-export default function Page({searchParams}:{
-    searchParams:{pageId?:string}
-}){
-
+export default function Page({searchParams}:{searchParams:{pageId:string}}){
 
     return(
         <>
+            <NotionUrlInput/>
 
-            <NotionPageContainer>
-                {/* NotionPageContainer에 왼쪽 퍼널이 포함되어 있음. Client Component와 Server Component 사용 위해*/}
-
-                <WriteFunnelContainer>
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <NotionPageContent
-                            pageId={searchParams.pageId}
-                        />           
-                    </Suspense> 
-                </WriteFunnelContainer>    
-
-            </NotionPageContainer>
-
-{/* 
-            {
-                isPending && 
-                <>
-                    <Backdrop open={true} sx={{zIndex:2000}}/>
-                    <CircularProgress color="primary" sx={{
-                        position:"fixed",
-                        top:"50%",
-                        left:"50%",
-                        zIndex:2001,
-                    }}/>
-                </>
-            }
-
-            {
-                isError && <ErrorSnackbar message={errorMessage}/>
-            } */}
-
+            <NotionPageContent pageId={searchParams.pageId}/>   
         </>
     )
 }
