3a4d18f750759e169d4f3edf1ddab2f46b6a6364
YoungCheon Kim <kyt031522@gmail.com>
2024-11-13 04:41:38 +0900

    token 확인용

--- middleware.ts
+++ middleware.ts
@@ -8,6 +8,8 @@ export const config = { matcher: ["/write"] };
 export async function middleware(request: NextRequest) {
 	const token = await getToken({ req: request });
 
+	console.log(token);
+
 	if (!token && process.env.NEXTAUTH_URL) {
 		return NextResponse.redirect(process.env.NEXTAUTH_URL);
 	}
