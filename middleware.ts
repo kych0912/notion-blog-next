cc81e2d226be79f607e8f0b0c74052d6e574938b
YoungCheon Kim <kyt031522@gmail.com>
2024-11-13 04:44:29 +0900

    getToken 옵션 변경

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,11 @@ export { default } from "next-auth/middleware";
 export const config = { matcher: ["/write"] };
 
 export async function middleware(request: NextRequest) {
-	const token = await getToken({ req: request });
+	const token = await getToken({ 
+		req: request,
+		secret: process.env.NEXTAUTH_SECRET,
+		secureCookie: true
+	});
 
 	console.log(token);
 
