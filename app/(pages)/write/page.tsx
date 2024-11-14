2dba0c539487431149d0b96944a9675603520245
YoungCheon Kim <kyt031522@gmail.com>
2024-11-14 23:50:42 +0900

    pageId 분리

--- app/(pages)/write/page.tsx
+++ app/(pages)/write/page.tsx
@@ -1,13 +1,23 @@
 import NotionPageContent from "./_pages/RightComponents/NotionPageContent";
 import NotionUrlInput from "./_pages/LeftComponents/NotionUrlInput";
+import ApiErrorBoundary from "@/app/components/Error/APIErrorBoundary";
+import NotionRecordMapFetcher from "@/app/components/Fetcher/NotionRecordMapFetcher";
 
-export default async function Page({searchParams}:{searchParams:{pageId:string}}){
+export default function Page({searchParams}:{searchParams:{pageId:string}}){
 
+    const {pageId} = searchParams;
+
+    //레이아웃 구성
     return(
         <>
-            <NotionUrlInput/>
 
-            <NotionPageContent pageId={searchParams.pageId}/>   
+            <NotionUrlInput />
+
+            <ApiErrorBoundary>
+                <NotionRecordMapFetcher pageId={pageId}>
+                    <NotionPageContent/>   
+                </NotionRecordMapFetcher>
+            </ApiErrorBoundary>
         </>
     )
 }
