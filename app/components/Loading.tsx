24e69e868ad354318f9dd558c4ff9c905c49ecc2
YoungCheon Kim <kyt031522@gmail.com>
2024-11-18 16:06:38 +0900

    Loading import수정

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 
 import { LoadingIcon } from './LoadingIcon'
-import styles from './styles.module.css'
+import './styles.module.css'
 
 export const Loading: React.FC = () => (
   <div className="loadingContainer">
