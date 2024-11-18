e35d60f71dd747433d9053ef415f6a5c730b1baa
YoungCheon Kim <kyt031522@gmail.com>
2024-11-18 16:04:53 +0900

    에러책임 전가

--- app/services/post/post.ts
+++ app/services/post/post.ts
@@ -2,63 +2,41 @@ import axios from "axios";
 
 interface PostData{
     notionUrl:string|undefined,
-    description:string|undefined,
 }
 
 export async function uploadPost(data:PostData){
-    try{
-        const body = {
-            notionUrl:data.notionUrl,
-            description:data.description
-        }
-
-        const res = await axios.post(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/post/write`,body,{
-            withCredentials: true 
-          });
-        return{
-            message:res.data.message,
-            isSuccess:res.data.isSuccess
-        }
+    const body = {
+        notionUrl:data.notionUrl,
     }
-    catch(err){
-        return err;
+
+    const res = await axios.post(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/post/write`,body,{
+        withCredentials: true 
+        });
+    return{
+        message:res.data.message,
+        isSuccess:res.data.isSuccess
     }
 }
 
 export async function getLatestPosts(){
-    try{
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/post/latest`);
-        return res.data;
-    }
-    catch(err){
-        return err;
-    }
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/post/latest`);
+    return res.data;
 }
 
 export async function getUserPosts(id:string){
-    try{
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/post/user/${id}`);
-        return res.data;
-    }
-    catch(err){
-        return err;
-    }
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/post/user/${id}`);
+    return res.data;
 }
 
 export async function getPostDetail(id:string,user:string,token:string){
-    try{
-        const config = {
-            headers: {
-              'X-Next-Auth-Session-Token': token,
-            }
-          };
-          
-          const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/post/${user}/${id}`, config);
-        return res.data;
-    }
-    catch(err){
-        return err;
-    }
+    const config = {
+        headers: {
+            'X-Next-Auth-Session-Token': token,
+        }
+        };
+        
+        const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/post/${user}/${id}`, config);
+    return res.data;
 }
 
 export async function deletePost(id:string){
