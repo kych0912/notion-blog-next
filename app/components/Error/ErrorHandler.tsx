2854330600165c88938bfd01d912d7be7dd7abd8
YoungCheon Kim <kyt031522@gmail.com>
2024-11-18 16:07:19 +0900

    에러 메세지 컴포넌트 분리

--- app/components/Error/ErrorHandler.tsx
+++ app/components/Error/ErrorHandler.tsx
@@ -1,54 +1,29 @@
 'use client'
-import React, { useState, useEffect } from 'react';
-import { Snackbar, Alert } from '@mui/material';
+import HelperText from "./_components/HelperText";
+import ErrorSnackbar from "./_components/SnackBar";
 
-interface ErrorProps {
+type ErrorType = "snackbar" | "alert" | "helperText";
+
+interface ErrorHandlerProps {
   message: string;
   duration?: number;
-  type?: "snackbar" | "alert";
-  resetError: () => void;
+  type?: ErrorType;
+  resetError?: () => void;
 }
-
-const ErrorSnackbar: React.FC<ErrorProps> = ({ message, duration = 3000, resetError}) => {
-  const [open, setOpen] = useState(false);
-
-  useEffect(() => {
-    if (message) {
-      setOpen(true);
-    } else {
-      setOpen(false);
-    }
-  }, [message]);
-
-  const handleClose = () => {
-    setOpen(false);
-    resetError();
-  };
-
-  return (
-    <Snackbar 
-      open={open} 
-      autoHideDuration={duration} 
-      onClose={handleClose} 
-      anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-    >
-      <Alert 
-        onClose={handleClose}
-        severity="error" 
-        variant="filled"
-      >
-        {message}
-      </Alert>
-    </Snackbar>
-  );
-};
 
 export default function ErrorHandler({ 
   message, 
   duration = 3000, 
   type,
   resetError
- }:ErrorProps){
-  if(type === "snackbar") return <ErrorSnackbar message={message} duration={duration} resetError={resetError}/>;
+ }:ErrorHandlerProps){
+  if(type === "snackbar") return (
+    <ErrorSnackbar 
+      message={message} 
+      duration={duration} 
+      resetError={resetError}
+    />
+  );
+  if(type === "helperText") return <HelperText message={message} />;  
   if(type === "alert") return <></>;  
 }
