0a8fb644c63ceaa09c22acd9cd19987ddee060c4
YoungCheon Kim <kyt031522@gmail.com>
2024-11-29 00:48:20 +0900

    fix: sql injection 방지를 위한 executeQuery 파라미터 사용

--- app/lib/UserData/UserDB.ts
+++ app/lib/UserData/UserDB.ts
@@ -4,10 +4,10 @@ import jwt from 'jsonwebtoken';
 const saltRounds = 10;
 
 export async function findUserByName(id:string){
-    const query = `SELECT * FROM user WHERE id="${id}"`;
+    const query = `SELECT * FROM user WHERE id=?`;
 
     try {
-        const data = await executeQuery(query, []);
+        const data = await executeQuery(query, [id]);
 
         if (data === null || (typeof data === 'object' && (data as any[]).length === 0)) {
             return null;
@@ -46,10 +46,10 @@ export async function createUser(id:string,password:string,avatar:string){
     const salt = await bcrypt.genSalt(saltRounds);
     const hashedPassword:string = await bcrypt.hash(password, salt);
 
-    const query = `INSERT INTO user(id, password,avatar) VALUES('${id}','${hashedPassword}','${avatar}')`;
+    const query = `INSERT INTO user(id, password,avatar) VALUES(?,?,?)`;
 
     try {
-        const data = await executeQuery(query, []);
+        const data = await executeQuery(query, [id,hashedPassword,avatar]);
         return data;
     } catch (err) {
         console.log(err);
@@ -58,10 +58,10 @@ export async function createUser(id:string,password:string,avatar:string){
 }
 
 export async function getUserInfo(id:string){
-    const query = `SELECT * FROM user WHERE id='${id}'`;
+    const query = `SELECT * FROM user WHERE id=?`;
 
     try {
-        const data = await executeQuery(query, []);
+        const data = await executeQuery(query, [id]);
         return data;
     } catch (err) {
         console.log(err);
