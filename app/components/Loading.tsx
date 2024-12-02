0395de13eb643b92bd0c373622f6d54c226615f7
YoungCheon Kim <kyt031522@gmail.com>
2024-12-02 16:20:40 +0900

    add: fixed loading component 추가

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,10 +1,29 @@
-import * as React from 'react'
+'use client'
+import { Box, CircularProgress } from '@mui/material';
 
-import { LoadingIcon } from './LoadingIcon'
-import './styles.module.css'
-
-export const Loading: React.FC = () => (
-  <div className="loadingContainer">
-    <LoadingIcon />
-  </div>
-)
+export const Loading = () => (
+  <Box
+    sx={{
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    }}
+  >
+    <CircularProgress 
+      sx={{
+        color: 'primary.main',
+        // 선택적: 크기 조절
+        '& .MuiCircularProgress-svg': {
+          width: 40,
+          height: 40
+        }
+      }}
+    />
+  </Box>
+);
