27e6bf4f940791a88cd5e24cd1d1f6f6e169952b
YoungCheon Kim <kyt031522@gmail.com>
2024-12-05 17:25:55 +0900

    refactor: detail page id,user 분리

--- app/(pages)/[user]/[id]/page.tsx
+++ app/(pages)/[user]/[id]/page.tsx
@@ -4,11 +4,13 @@ import { cookies } from "next/headers";
 async function Page({params}: {params:{id:string,user:string }}) {    
     const cookieStore = cookies();
     const token = cookieStore.get(process.env.NEXTAUTH_COOKIE_NAME as string)?.value ?? '';
+    const id = params.id;
+    const user = params.user;
 
     return (
         <DetailContent 
-            id={params.id} 
-            user={params.user} 
+            id={id} 
+            user={user} 
             token={token}
         />
     );
