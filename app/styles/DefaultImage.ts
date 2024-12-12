335f9de28c0f9484fa8fda8b9878b8f5a24efd2d
YoungCheon Kim <kyt031522@gmail.com>
2024-12-12 23:28:45 +0900

    chore:Default Image 삭제 - 개발 때 사용했던 default Image 삭제

--- app/styles/DefaultImage.ts
+++
@@ -1,1 +0,0 @@
-export const DEFAULT_IMAGE = `https://transitivebullsh.it/_next/image?url=https%3A%2F%2Fwww.notion.so%2Fimage%2Fhttps%253A%252F%252Fs3-us-west-2.amazonaws.com%252Fsecure.notion-static.com%252Ff70d3dc6-ce97-4be2-9cde-b86606147b41%252F06.jpg%3Ftable%3Dblock%26id%3D78fc5a4b-88d7-4b0e-824e-29407e9f1ec1%26cache%3Dv2&w=2048&q=75`
