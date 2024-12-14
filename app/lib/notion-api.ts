1870a496f603909783a8fbbf406df8556778c266
YoungCheon Kim <kyt031522@gmail.com>
2024-12-14 23:23:21 +0900

    refactor:notion util함수 분리 - getNotionImage, getPageBlockContent 분리 - DB사용 안하는 유틸 함수들

--- app/lib/notion-api.ts
+++ app/lib/notion-api.ts
@@ -2,12 +2,11 @@ import { NotionAPI } from 'notion-client'
 import * as types from 'notion-types'
 import { getPageTitle, getBlockParentPage } from 'notion-utils'
 import { getPostById } from './postData/postDB'
-import { NextResponse } from 'next/server'
-
-const notion = new NotionAPI()
+import { getNotionImage } from '@/app/utils/NotionApi'
 
 export async function getPage(id:string) {
     try {
+        const notion = new NotionAPI()
         return await notion.getPage(id)
     } catch(err) {
         throw {
@@ -17,47 +16,6 @@ export async function getPage(id:string) {
                 isSuccess: false
             }
         };
-    }
-}
-
-export function getNotionImage(url:string,keys:string[], block:types.Block){
-    if (!url) {
-        return null;
-    }
-
-    const baseUrl = 'https://www.notion.so';
-    const isImagePath = url.startsWith('/image');
-    const encodedUrl = encodeURIComponent(url);
-    
-    if (url.startsWith('/')) {
-        url = `${baseUrl}${isImagePath ? url : `/image/${encodedUrl}`}`;
-        return url;
-    } else {
-        url = `${baseUrl}${isImagePath ? url : `/image/${encodedUrl}`}`;
-    }
-
-    // URL 객체 생성 및 검색 매개변수 설정
-    const notionImageUrl = new URL(url);
-    let table = block.parent_table === 'space' ? 'block' : block.parent_table;
-    
-    if (table === 'collection' || table === 'team') {
-        table = 'block';
-    }
-    
-    notionImageUrl.searchParams.set('table', table);
-    notionImageUrl.searchParams.set('id', keys[0]);
-    notionImageUrl.searchParams.set('cache', 'v2');
-
-    return notionImageUrl.toString();
-}
-
-export function getPageBlockContent(recordMap:types.RecordMap, keys:string[]){
-    for(let i = 1;i<keys.length;i++){
-        const block = recordMap?.block?.[keys[i]]?.value.properties?.title[0][0];
-        const type = recordMap?.block?.[keys[i]]?.value.type;
-        if(block && type === 'text'){
-            return block;
-        }
     }
 }
 
---
+++ app/utils/NotionApi.ts
@@ -0,0 +1,42 @@
+import * as types from 'notion-types'
+
+export function getNotionImage(url:string,keys:string[], block:types.Block){
+    if (!url) {
+        return null;
+    }
+
+    const baseUrl = 'https://www.notion.so';
+    const isImagePath = url.startsWith('/image');
+    const encodedUrl = encodeURIComponent(url);
+    
+    if (url.startsWith('/')) {
+        url = `${baseUrl}${isImagePath ? url : `/image/${encodedUrl}`}`;
+        return url;
+    } else {
+        url = `${baseUrl}${isImagePath ? url : `/image/${encodedUrl}`}`;
+    }
+
+    // URL 객체 생성 및 검색 매개변수 설정
+    const notionImageUrl = new URL(url);
+    let table = block.parent_table === 'space' ? 'block' : block.parent_table;
+    
+    if (table === 'collection' || table === 'team') {
+        table = 'block';
+    }
+    
+    notionImageUrl.searchParams.set('table', table);
+    notionImageUrl.searchParams.set('id', keys[0]);
+    notionImageUrl.searchParams.set('cache', 'v2');
+
+    return notionImageUrl.toString();
+}
+
+export function getPageBlockContent(recordMap:types.RecordMap, keys:string[]){
+    for(let i = 1;i<keys.length;i++){
+        const block = recordMap?.block?.[keys[i]]?.value.properties?.title[0][0];
+        const type = recordMap?.block?.[keys[i]]?.value.type;
+        if(block && type === 'text'){
+            return block;
+        }
+    }
+}
