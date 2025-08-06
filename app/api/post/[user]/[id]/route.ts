8619e67cef3ef1031af565375feb854b11ac8170
YoungCheon <kyt031522@gmail.com>
2025-08-07 00:35:26 +0900

    💚 Build error 해결

--- app/api/post/[user]/[id]/route.ts
+++ app/api/post/[user]/[id]/route.ts
@@ -1,73 +1,106 @@
-export const dynamic = 'force-dynamic';
-import {NextRequest} from 'next/server';
-import { NextResponse } from 'next/server';
-import { getPostDetail } from '@/app/lib/postData/postDB';
-import { decode } from 'next-auth/jwt';
-import { headers } from 'next/headers';
-import { isDescendantOfStoredPage } from '@/app/lib/notion-api';
+export const dynamic = "force-dynamic";
+import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { getPostDetail } from "@/app/lib/postData/postDB";
+import { decode } from "next-auth/jwt";
+import { headers } from "next/headers";
+import { isDescendantOfStoredPage } from "@/app/lib/notion-api";
 
-export async function GET(req:NextRequest, { params }: { params: { id: string, user:string } }){
-    const id = params.id;
-    const user = params.user;
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string; user: string } }
+) {
+  const id = params.id;
+  const user = params.user;
 
-    //secret 값 default 설정
-    const secret = process.env.NEXTAUTH_SECRET || '';
+  //secret 값 default 설정
+  const secret = process.env.NEXTAUTH_SECRET || "";
 
-    const headerList = headers();
+  const headerList = headers();
 
-    //header에서 encoded token을 가져옴
-    const rawToken = headerList.get('x-next-auth-session-token') || headerList.get('x-next-auth.session-token');
+  //header에서 encoded token을 가져옴
+  const rawToken =
+    headerList.get("x-next-auth-session-token") ||
+    headerList.get("x-next-auth.session-token");
 
-    let isAuthor = false;
+  let isAuthor = false;
 
-    try{
-        const res = await getPostDetail(id,user);
+  try {
+    const res = await getPostDetail(id, user);
 
-        //자식 페이지가 존재하지 않을 때
-        //부모 페이지로 올라가며 db에서 id 탐색
-        //없으면 404 반환
-        
+    //자식 페이지가 존재하지 않을 때
+    //부모 페이지로 올라가며 db에서 id 탐색
+    //없으면 404 반환
 
-        //포스트가 존재하지 않을 때
-        if(!Array.isArray(res) || res.length === 0){  
-            //자식 페이지인지 확인
-            const isChildPage = await isDescendantOfStoredPage(id);
+    //포스트가 존재하지 않을 때
+    if (!Array.isArray(res) || res.length === 0) {
+      //자식 페이지인지 확인
+      const isChildPage = await isDescendantOfStoredPage(id);
 
-            //자식 페이지도 아니면 404
-            if(!isChildPage){
-                return NextResponse.json({message:"Post Not Found",isSuccess:false},{status:404})
-            }
-            
-            //자식 페이지일 경우 페이지 return
-            return NextResponse.json({data:{
-                id:id,
-            },isAuthor:isAuthor,isChild:true,isSuccess:true},{status:200})
-        }
+      //자식 페이지도 아니면 404
+      if (!isChildPage) {
+        return NextResponse.json(
+          { message: "Post Not Found", isSuccess: false },
+          { status: 404 }
+        );
+      }
 
-        //토큰 유효성 검사
-        if(!rawToken){
-            return NextResponse.json({data:res,isAuthor:isAuthor,isChild:false,isSuccess:true},{status:200})
-        }
-
-        const decodedToken = await decode({token:rawToken,secret}); 
-
-        //토큰이 유효하지 않을 때
-        if(!decodedToken){
-            return NextResponse.json({data:res,isAuthor:isAuthor,isChild:false,isSuccess:true},{status:200})
-        }
-        
-        if(decodedToken.name === user){
-            isAuthor = true;
-        }
-
-        return NextResponse.json({data:res,isAuthor:isAuthor,isChild:false,isSuccess:true},{status:200})
+      //자식 페이지일 경우 페이지 return
+      return NextResponse.json(
+        {
+          data: {
+            id: id,
+          },
+          isAuthor: isAuthor,
+          isChild: true,
+          isSuccess: true,
+        },
+        { status: 200 }
+      );
     }
-    catch(err){
-        if(err instanceof Error){
-            if(err.message.includes("invalid notion pageId")){
-                return NextResponse.json({message:"Not Found",isSuccess:false},{status:404})
-            }
-        }
-        return NextResponse.json({message:"Internal Server Error",isSuccess:false},{status:500})
+
+    //토큰 유효성 검사
+    if (!rawToken) {
+      return NextResponse.json(
+        { data: res, isAuthor: isAuthor, isChild: false, isSuccess: true },
+        { status: 200 }
+      );
     }
+
+    const decodedToken = await decode({
+      token: rawToken,
+      secret,
+      salt: secret,
+    });
+
+    //토큰이 유효하지 않을 때
+    if (!decodedToken) {
+      return NextResponse.json(
+        { data: res, isAuthor: isAuthor, isChild: false, isSuccess: true },
+        { status: 200 }
+      );
+    }
+
+    if (decodedToken.name === user) {
+      isAuthor = true;
+    }
+
+    return NextResponse.json(
+      { data: res, isAuthor: isAuthor, isChild: false, isSuccess: true },
+      { status: 200 }
+    );
+  } catch (err) {
+    if (err instanceof Error) {
+      if (err.message.includes("invalid notion pageId")) {
+        return NextResponse.json(
+          { message: "Not Found", isSuccess: false },
+          { status: 404 }
+        );
+      }
+    }
+    return NextResponse.json(
+      { message: "Internal Server Error", isSuccess: false },
+      { status: 500 }
+    );
+  }
 }
--- app/services/user/user.ts
+++ app/services/user/user.ts
@@ -1,92 +1,108 @@
-import axios from 'axios';
-import { AxiosError } from 'axios';
+import axios from "axios";
+import { AxiosError } from "axios";
 
-interface TAuthReturn{
-    id:string,
-    isLogged:boolean,
-    message:string,
-    user:{
-        name:string,
-        avatar_url:string
+interface TAuthReturn {
+  id: string;
+  isLogged: boolean;
+  message: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+}
+
+export const getAuth = async (): Promise<TAuthReturn> => {
+  const res = await axios.get(
+    `${process.env.NEXT_PUBLIC_BASE_API_URL}/api/user/auth`,
+    {
+      headers: { "Content-Type": "multipart/form-data" },
+      withCredentials: true,
     }
-}
+  );
+  return {
+    id: res.data.id,
+    isLogged: res.data.isLogged,
+    message: res.data.message,
+    user: res.data.user,
+  };
+};
 
-export const getAuth = async ():Promise<TAuthReturn>=> {    
-    const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/user/auth`,{ headers: { "Content-Type": "multipart/form-data" },withCredentials: true });
-    return {
-        id: res.data.id,
-        isLogged: res.data.isLogged,
-        message: res.data.message,
-        user: res.data.user   
-    };
-}
-
-export const login = async (data:{
-        id:string,
-        password:string
-    }) => {
-    try {
-        const res = await axios.post(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/user/login`, data);
-        return res;
-    } catch (err) {
-        return err;
-    }
-}
+export const login = async (data: { id: string; password: string }) => {
+  try {
+    const res = await axios.post(
+      `${process.env.NEXT_PUBLIC_BASE_API_URL}/api/user/login`,
+      data
+    );
+    return res;
+  } catch (err) {
+    return err;
+  }
+};
 
 export const logout = async () => {
-    try {
-
-        const res = await axios.get(`/api/user/logout`,{ headers: { "Content-Type": "multipart/form-data" },withCredentials: true });
-        return res;
-    } catch (err) {
-        return err;
-    }
-}
-
-export const updateUser = async (id:string,name:string,avatar_url:string,email:string) => {
-    const res = await axios.put(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/user/updateUser`,{
-        id:id,
-        name,
-        avatar_url,
-        email
+  try {
+    const res = await axios.get(`/api/user/logout`, {
+      headers: { "Content-Type": "multipart/form-data" },
+      withCredentials: true,
     });
-    const _data = res.data;
+    return res;
+  } catch (err) {
+    return err;
+  }
+};
 
-    //server component에서 client component로 데이터 전달 시, Promise 객체를 전달한다.
-    //하지만 Promise 객체는 JSON.stringify를 사용할 수 없다.
-    //nextjs 의 특징
-    //따라서, JSON.stringify를 사용하려면, Promise 객체를 data 객체로 변환해야한다.
-    return _data;
-}
-
-export const getUserInfoAndPostByName = async (name:string) =>{
-    const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/user?name=${name}`);
-    return res.data;
-}
-
-export const getUserInfoById = async (id:string)=>{
-    try{    
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/user/${id}`);
-        return res.data.data;
+export const updateUser = async (
+  id: string | undefined,
+  name: string,
+  avatar_url: string,
+  email: string
+) => {
+  const res = await axios.put(
+    `${process.env.NEXT_PUBLIC_BASE_API_URL}/api/user/updateUser`,
+    {
+      id: id,
+      name,
+      avatar_url,
+      email,
     }
-    catch(err:any){
-        if(err.response?.status === 404){
-            return [];
-        }
-        throw err;
-    }
-}
+  );
+  const _data = res.data;
 
+  //server component에서 client component로 데이터 전달 시, Promise 객체를 전달한다.
+  //하지만 Promise 객체는 JSON.stringify를 사용할 수 없다.
+  //nextjs 의 특징
+  //따라서, JSON.stringify를 사용하려면, Promise 객체를 data 객체로 변환해야한다.
+  return _data;
+};
+
+export const getUserInfoAndPostByName = async (name: string) => {
+  const res = await axios.get(
+    `${process.env.NEXT_PUBLIC_BASE_API_URL}/api/user?name=${name}`
+  );
+  return res.data;
+};
+
+export const getUserInfoById = async (id: string | undefined) => {
+  try {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_BASE_API_URL}/api/user/${id}`
+    );
+    return res.data.data;
+  } catch (err: any) {
+    if (err.response?.status === 404) {
+      return [];
+    }
+    throw err;
+  }
+};
 
 axios.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    async (error: AxiosError) => {
-        if (error.response?.status === 401) {
-            
-            
-        }
-        return Promise.reject(error);
+  (response) => {
+    return response;
+  },
+  async (error: AxiosError) => {
+    if (error.response?.status === 401) {
     }
+    return Promise.reject(error);
+  }
 );
