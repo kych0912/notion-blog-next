054b817e82f6da5fd597d12d526d6c709e384219
kych0912 <kyt031522@gmail.com>
2025-12-29 18:43:59 +0900

    홈 배경색 변경

--- app/components/Layout/HomeLayout.tsx
+++ app/components/Layout/HomeLayout.tsx
@@ -1,6 +1,6 @@
 export const HomePageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex min-h-screen flex-col items-center bg-background">
+    <div className="flex min-h-screen flex-col items-center bg-muted/20">
       <div className="mx-auto w-full px-2 md:max-w-[900px] lg:max-w-[1200px]">{children}</div>
     </div>
   );
