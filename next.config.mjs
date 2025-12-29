d409ce3aaee373094f1394b69a329e05fcdbda21
kych0912 <kyt031522@gmail.com>
2025-12-29 16:27:05 +0900

    next 업데이트 및 불필요한 라우트 삭제

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,15 @@ const nextConfig = {
   poweredByHeader: false,
   images: {
     domains: [
-      "www.notion.so",
-      "transitivebullsh.it",
-      "www.notion.so",
-      "notion.so",
-      "images.unsplash.com",
-      "prod-files-secure.s3.us-west-2.amazonaws.com",
-      "s3.us-west-2.amazonaws.com",
+      'www.notion.so',
+      'transitivebullsh.it',
+      'www.notion.so',
+      'notion.so',
+      'images.unsplash.com',
+      'prod-files-secure.s3.us-west-2.amazonaws.com',
+      's3.us-west-2.amazonaws.com',
     ],
-    formats: ["image/avif", "image/webp"],
+    formats: ['image/avif', 'image/webp'],
   },
   reactStrictMode: false,
 
@@ -20,24 +20,24 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: '/api/:path*',
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
           {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
           },
           {
-            key: "Access-Control-Allow-Headers",
+            key: 'Access-Control-Allow-Headers',
             value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
           },
           {
-            key: "Access-Control-Allow-Origin",
+            key: 'Access-Control-Allow-Origin',
             value:
-              process.env.NODE_ENV === "production"
-                ? "https://nextblog.me"
-                : "http://localhost:3000",
+              process.env.NODE_ENV === 'production'
+                ? 'https://nextblog.me'
+                : 'http://localhost:3000',
           },
         ],
       },
