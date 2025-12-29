73ad3952886f4eb32f0e16dcc4a146892e98ef42
kych0912 <kyt031522@gmail.com>
2025-12-29 15:56:10 +0900

    불필요한 패키지 삭제

--- app/server/queries/user.ts
+++ app/server/queries/user.ts
@@ -1,12 +1,9 @@
 'use server';
-import bcrypt from 'bcryptjs';
 import { eq } from 'drizzle-orm';
 
 import * as schema from '@/app/server/db/schema';
 import { db } from '@/app/server/db/drizzle';
 import type { UserType } from '@/app/server/db/schema';
-
-const saltRounds = 10;
 
 export async function findUserByName(name: string) {
   try {
@@ -14,37 +11,6 @@ export async function findUserByName(name: string) {
 
     return data[0] || null;
   } catch (err) {
-    throw err;
-  }
-}
-
-export async function comparePassword(password: string, hash: string) {
-  try {
-    const isMatch = await bcrypt.compare(password, hash);
-    return isMatch;
-  } catch (err: unknown) {
-    throw err;
-  }
-}
-
-export async function createUser(
-  id: string,
-  name: string,
-  password: string,
-  avatar: string,
-  email: string,
-) {
-  try {
-    const salt = await bcrypt.genSalt(saltRounds);
-    const hashedPassword: string = await bcrypt.hash(password, salt);
-
-    const data = await db
-      .insert(schema.user)
-      .values({ id, name, password: hashedPassword, avatar, email })
-      .returning();
-
-    return data;
-  } catch (err: unknown) {
     throw err;
   }
 }
