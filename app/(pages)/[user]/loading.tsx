3adb0ed25202bc249fb8549ffc9d5a2fd83a9b05
kych0912 <kyt031522@gmail.com>
2026-01-01 16:07:44 +0900

    중첩 loading 처리

--- app/(pages)/[user]/loading.tsx
+++ app/(pages)/[user]/loading.tsx
@@ -1,5 +1,13 @@
+'use client';
+import { usePathname } from 'next/navigation';
+
 import UserPageSkeleton from './_components/UserPageSkeleton';
 
 export default function Loading() {
+  const pathname = usePathname();
+  const isNestedRoute = pathname.split('/').length > 2;
+
+  if (isNestedRoute) return undefined;
+
   return <UserPageSkeleton />;
 }
